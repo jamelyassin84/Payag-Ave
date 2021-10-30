f6cee53d33a736d6de9ffe50f48e46de04f82efb
Jamel Yassin <jamelyassin84@gmail.com>
2021-10-30 13:45:20 +0400

    modal-initial

--- src/app/components/right-modal/right-modal.component.spec.ts
+++ src/app/components/right-modal/right-modal.component.spec.ts
@@ -35,21 +35,48 @@ describe('RightModalComponent', () => {
 
 	it('should  close if ESC key is pressed.', () => {})
 
-	it('should have modal-content', () => {})
+	it('should have modal-content', () => {
+		debugElement = fixture.debugElement.query(
+			By.css('.modal-content')
+		).nativeElement
+		expect(debugElement).toBeTruthy()
+	})
 
 	it('should should be on left.', () => {})
 
-	it('should  have a header.', () => {})
+	it('should  have a header.', () => {
+		debugElement = fixture.debugElement.query(
+			By.css('.modal-header')
+		).nativeElement
+		expect(debugElement).toBeTruthy()
+	})
 
-	it('should  have a title.', () => {})
+	it('should  have a modal-title.', () => {
+		debugElement = fixture.debugElement.query(
+			By.css('.modal-title')
+		).nativeElement
+		expect(debugElement).toBeTruthy()
+	})
 
-	it('should have close button', () => {})
+	it('should have close button', () => {
+		debugElement = fixture.debugElement.query(
+			By.css('.close-btn')
+		).nativeElement
+		expect(debugElement).toBeTruthy()
+	})
 
-	it('should have a modal body', () => {})
+	it('should have a modal body', () => {
+		debugElement = fixture.debugElement.query(
+			By.css('.modal-body')
+		).nativeElement
+		expect(debugElement).toBeTruthy()
+	})
 
 	it('should have a template outlet', () => {})
 
 	it('should have a class attribute', () => {})
+
+	it('should have a modal-footer', () => {})
 
 	it('should have a callback button', () => {})
 
--- src/app/components/right-modal/right-modal.component.ts
+++ src/app/components/right-modal/right-modal.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core'
 
 @Component({
-  selector: 'app-right-modal',
-  templateUrl: './right-modal.component.html',
-  styleUrls: ['./right-modal.component.scss']
+	selector: 'app-right-modal',
+	templateUrl: './right-modal.component.html',
+	styleUrls: ['./right-modal.component.scss'],
 })
 export class RightModalComponent implements OnInit {
+	@Input() body: any
+	@Input() title: string = 'Modal Title'
+	@Input() actionButtonName: string = 'Save'
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
